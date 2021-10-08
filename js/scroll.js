c726d13c95bdc409b32cf02d75285c57ccbb78c6
Marcus Urani <marcus.urani20@gmail.com>
2021-10-08 11:16:19 -0300

    Finalizando a melhoria do código

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-$('.comunicado__lista a').click(function(e){
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			menuHeight = $('ul').innerHeight(),
-			targetOffset = $(id).offset().top;
-	$('html, body').animate({
-		scrollTop: targetOffset - menuHeight
-	}, 500);
-});
